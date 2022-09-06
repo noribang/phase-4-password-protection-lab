59f898a076ffae1860767401fe3d96902facf8f5
hoonkim <hoonk9387@gmail.com>
2022-09-05 22:03:10 -0500

    Create Users controller create method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+    before_action :authorize, only: [:show]
 
     def create  
+        user = User.create(user_params)
 
+    end
+
+    private
+
+    def authorize
+        return render json: { error:"Not authorized" }, status: :unauthorized unless session.include? :user_id
+    end
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
     end
 end
